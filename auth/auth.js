15f2ba7370b762374ab08cc33c045b7e6e401ea9
Felipe Morales <felipe.morales.querol@gmail.com>
2023-04-28 22:18:45 -0300

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -40,7 +40,7 @@ passport.use('login', new localStrategy({
 }))
 
 passport.use(new JWTStrategy({
-    secretOrKey: 'top_secretfga345S:LJLFJFDk',
+    secretOrKey: 'top_secret123456',
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken('token')
 }, async (token, done) => {
     try {
